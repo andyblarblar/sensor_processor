30390d64c9c06ae90a1286998a6d4748772832af
girvenavery2022 <girvenavery@gmail.com>
2022-02-14 13:08:58 -0500

    added hpp file

--- src/pointcloud.cpp
+++ src/pointcloud.cpp
@@ -1,104 +1,73 @@
-#include <memory>
-#include <string>
-#include <optional>
 
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
+#include "pointcloud_merge/pointcloud.hpp"
+
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/conversions.h>
-#include "tf2_ros/buffer.h"
-#include "tf2_ros/transform_listener.h"
 
-using namespace message_filters;
-
-class PointCloudMerge : public rclcpp::Node
+PointCloudMerge::PointCloudMerge(rclcpp::NodeOptions options)
+: Node("PointCloud_Concatenate", options)
 {
-public:
-  PointCloudMerge(rclcpp::NodeOptions options) : Node("PointCloud_Concatenate", options)
-  {
-    // Params
-    camera_trans_source = this->declare_parameter("camera_trans_source", "base_footprint");
-    camera_trans_dest = this->declare_parameter("camera_trans_dest", "laser_link");
-    tf_timeout = this->declare_parameter("tf_timeout", 0.03);
+  // Params
+  camera_trans_source = this->declare_parameter("camera_trans_source", "base_footprint");
+  camera_trans_dest = this->declare_parameter("camera_trans_dest", "laser_link");
+  tf_timeout = this->declare_parameter("tf_timeout", 0.03);
     
-    // Tf2
-    tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-    transform_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);  
+  // Tf2
+  tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  transform_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);  
        
-    // message filter synchronizer and publisher
-    rmw_qos_profile_t rmw_qos_profile = rmw_qos_profile_sensor_data;
-    lidar_subscription_.subscribe(this, "/lidar/points", rmw_qos_profile);
-    camera_subscription_.subscribe(this, "/camera/points", rmw_qos_profile);
-    sync.reset(new Sync(MySyncPolicy(10), lidar_subscription_, camera_subscription_));
+  // message filter synchronizer and publisher
+  rmw_qos_profile_t rmw_qos_profile = rmw_qos_profile_sensor_data;
+  lidar_subscription_.subscribe(this, "/lidar/points", rmw_qos_profile);
+  camera_subscription_.subscribe(this, "/camera/points", rmw_qos_profile);
+  sync.reset(new Sync(MySyncPolicy(10), lidar_subscription_, camera_subscription_));
 
-    // synchronizer's callback function
-    sync->registerCallback(std::bind(&PointCloudMerge::pc_callback, this, std::placeholders::_1, std::placeholders::_2));
+  // synchronizer's callback function
+  sync->registerCallback(std::bind(&PointCloudMerge::pc_callback, this, std::placeholders::_1, std::placeholders::_2));
 
-    combined_pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-        "/combined/points", rclcpp::SensorDataQoS());
-  }
+  combined_pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+    "/combined/points", rclcpp::SensorDataQoS());
+}
 
-private:
-  /*
-  Function that takes in 2 pointcloud messages
-  and concatenates them together then publishes
-  it on a single topic
-  */
-  void pc_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr &msg1, const sensor_msgs::msg::PointCloud2::ConstSharedPtr &msg2)
+/*
+Function that takes in 2 pointcloud messages
+and concatenates them together then publishes
+it on a single topic
+*/
+void PointCloudMerge::pc_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr &msg1, const sensor_msgs::msg::PointCloud2::ConstSharedPtr &msg2)
+{
+  if (!camera_trans.has_value())
   {
-    if (!camera_trans.has_value())
-    {
-      camera_trans = tf_buffer->lookupTransform(camera_trans_dest, camera_trans_source, this->now(),
-      	rclcpp::Duration::from_seconds(tf_timeout));
-    }
-
-    auto &camera_trans_ = camera_trans.value();
-
-    pcl::PointCloud<pcl::PointXYZ> lidar, camera, merged;
-
-    pcl::fromROSMsg(*msg1, lidar);
-    pcl::fromROSMsg(*msg2, camera);
-
-    // Translate all points from the camera to move it into the dest frame.
-    // This is useful to move the camera into the laser_link frame, which is
-    // what the concatinated point cloud is published to.
-    for (auto &point : camera)
-    {
-      point.x += camera_trans_.transform.translation.x;
-      point.y += camera_trans_.transform.translation.y;
-      // Doing the z transform causes pcl-ls to fail, so we don't do it.
-    }
-
-    merged = lidar + camera;
-
-    sensor_msgs::msg::PointCloud2::SharedPtr output(new sensor_msgs::msg::PointCloud2);
-    pcl::toROSMsg(merged, *output);
-
-    output->header.stamp = this->get_clock()->now();
-    combined_pc_publisher_->publish(*output);
+    camera_trans = tf_buffer->lookupTransform(camera_trans_dest, camera_trans_source, this->now(),
+      rclcpp::Duration::from_seconds(tf_timeout));
   }
-  
-  // message filter stuff
-  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> lidar_subscription_;
-  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> camera_subscription_;
-  typedef sync_policies::ApproximateTime<sensor_msgs::msg::PointCloud2, sensor_msgs::msg::PointCloud2> MySyncPolicy;
-  typedef Synchronizer<MySyncPolicy> Sync;
-  std::shared_ptr<Sync> sync;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr combined_pc_publisher_;
 
-  // Tf stuff
-  std::shared_ptr<tf2_ros::TransformListener> transform_listener{nullptr};
-  std::unique_ptr<tf2_ros::Buffer> tf_buffer;
-  float tf_timeout;
-  /// Transform to apply to the camera points
-  std::optional<geometry_msgs::msg::TransformStamped> camera_trans{};
-  std::string camera_trans_source;
-  std::string camera_trans_dest;
-};
+  auto &camera_trans_ = camera_trans.value();
+
+  pcl::PointCloud<pcl::PointXYZ> lidar, camera, merged;
+
+  pcl::fromROSMsg(*msg1, lidar);
+  pcl::fromROSMsg(*msg2, camera);
+
+  // Translate all points from the camera to move it into the dest frame.
+  // This is useful to move the camera into the laser_link frame, which is
+  // what the concatinated point cloud is published to.
+  for (auto &point : camera)
+  {
+    point.x += camera_trans_.transform.translation.x;
+    point.y += camera_trans_.transform.translation.y;
+    // Doing the z transform causes pcl-ls to fail, so we don't do it.
+  }
+
+  merged = lidar + camera;
+
+  sensor_msgs::msg::PointCloud2::SharedPtr output(new sensor_msgs::msg::PointCloud2);
+  pcl::toROSMsg(merged, *output);
+
+  output->header.stamp = this->get_clock()->now();
+  combined_pc_publisher_->publish(*output);
+}
 
 int main(int argc, char *argv[])
 {
