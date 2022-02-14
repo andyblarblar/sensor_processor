fc073b99e679d4577b7df93a4fe201e67aa817e7
girvenavery2022 <girvenavery@gmail.com>
2022-02-13 20:23:24 -0500

    oops

--- src/pointcloud.cpp
+++ src/pointcloud.cpp
@@ -17,61 +17,61 @@ using namespace message_filters;
 class PointCloudMerge : public rclcpp::Node
 {
 public: 
-	PointCloudMerge(rclcpp::NodeOptions options): Node("PointCloud_Concatenate", options)
-	{
-	
-	rmw_qos_profile_t rmw_qos_profile = rmw_qos_profile_sensor_data;
-	lidar_subscription_.subscribe(this, "/kohm/filtered_points", rmw_qos_profile);
-	camera_subscription_.subscribe(this, "/kohm/camera_points", rmw_qos_profile);
+  PointCloudMerge(rclcpp::NodeOptions options): Node("PointCloud_Concatenate", options)
+  {
+    
+    rmw_qos_profile_t rmw_qos_profile = rmw_qos_profile_sensor_data;
+    lidar_subscription_.subscribe(this, "/kohm/filtered_points", rmw_qos_profile);
+    camera_subscription_.subscribe(this, "/kohm/camera_points", rmw_qos_profile);
 
-	sync.reset(new Sync(MySyncPolicy(10), lidar_subscription_, camera_subscription_)); 
-	
-	// synchronizer's callback function 
-	sync -> registerCallback(std::bind(&PointCloudMerge::pc_callback, this, std::placeholders::_1, std::placeholders::_2));  
-	
-	combined_pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
-		"/combined/points", rclcpp::SensorDataQoS());  
-	}
-	
+    sync.reset(new Sync(MySyncPolicy(10), lidar_subscription_, camera_subscription_)); 
+    
+    // synchronizer's callback function 
+    sync -> registerCallback(std::bind(&PointCloudMerge::pc_callback, this, std::placeholders::_1, std::placeholders::_2));  
+    
+    combined_pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+      "/combined/points", rclcpp::SensorDataQoS());  
+  }
+    
 private:
 
-	/*
-	Function that takes in 2 pointcloud messages 
-	and concatenates them together then publishes
-	it on a single topic
-	*/
-	void pc_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr& msg1, const sensor_msgs::msg::PointCloud2::ConstSharedPtr& msg2) 
-	{
-		pcl::PointCloud<pcl::PointXYZ> lidar, camera, merged;
+  /*
+  Function that takes in 2 pointcloud messages 
+  and concatenates them together then publishes
+  it on a single topic
+  */
+  void pc_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr& msg1, const sensor_msgs::msg::PointCloud2::ConstSharedPtr& msg2) 
+  {
+    pcl::PointCloud<pcl::PointXYZ> lidar, camera, merged;
 
-		pcl::fromROSMsg(*msg1, lidar);
-		pcl::fromROSMsg(*msg2, camera);
-		
-		merged = lidar + camera;
-		
-		sensor_msgs::msg::PointCloud2::SharedPtr output(new sensor_msgs::msg::PointCloud2);
-		pcl::toROSMsg(merged, *output);
-		combined_pc_publisher_->publish(*output);
+    pcl::fromROSMsg(*msg1, lidar);
+    pcl::fromROSMsg(*msg2, camera);
+        
+    merged = lidar + camera;
+        
+    sensor_msgs::msg::PointCloud2::SharedPtr output(new sensor_msgs::msg::PointCloud2);
+    pcl::toROSMsg(merged, *output);
+    combined_pc_publisher_->publish(*output);
 
-	}
-	
-	message_filters::Subscriber<sensor_msgs::msg::PointCloud2> lidar_subscription_;
-	message_filters::Subscriber<sensor_msgs::msg::PointCloud2> camera_subscription_;
-	
-	typedef sync_policies::ApproximateTime<sensor_msgs::msg::PointCloud2, sensor_msgs::msg::PointCloud2> MySyncPolicy;
-	typedef Synchronizer<MySyncPolicy> Sync;
-	std::shared_ptr<Sync> sync;
-	rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr combined_pc_publisher_; 
+  }
+    
+  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> lidar_subscription_;
+  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> camera_subscription_;
+    
+  typedef sync_policies::ApproximateTime<sensor_msgs::msg::PointCloud2, sensor_msgs::msg::PointCloud2> MySyncPolicy;
+  typedef Synchronizer<MySyncPolicy> Sync;
+  std::shared_ptr<Sync> sync;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr combined_pc_publisher_; 
 };
 
 int main(int argc, char *argv[])
 {  
-		rclcpp::init(argc, argv);
-		rclcpp::executors::SingleThreadedExecutor exec;
-		rclcpp::NodeOptions options;
-		auto sensor_node = std::make_shared<PointCloudMerge>(options);
-		exec.add_node(sensor_node);
-		exec.spin();
-		rclcpp::shutdown();
-		return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  rclcpp::NodeOptions options;
+  auto sensor_node = std::make_shared<PointCloudMerge>(options);
+  exec.add_node(sensor_node);
+  exec.spin();
+  rclcpp::shutdown();
+  return 0;
 }
