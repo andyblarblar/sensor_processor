fdac176188a0331028c270fc827b33c7cb5083e0
JordanKra <jkrasan@umich.edu>
2022-03-30 22:15:40 -0400

    Added scan and depth repub

--- include/sensor_processor/sensor_processor.hpp
+++ include/sensor_processor/sensor_processor.hpp
@@ -7,6 +7,7 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "std_msgs/msg/header.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 namespace SensorProcessor
 {
@@ -19,11 +20,17 @@ namespace SensorProcessor
     private:
         //image_raw
         void raw_img_raw_callback(const sensor_msgs::msg::Image::SharedPtr msg);
-        void raw_imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);       
+        void raw_imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);    
+        void raw_laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg); 
+        void raw_depth_img_raw_callback(const sensor_msgs::msg::Image::SharedPtr msg);  
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr raw_img_raw_subscription_;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr unfiltered_img_raw_publisher_;
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr raw_depth_img_raw_subscription_;
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr unfiltered_depth_img_raw_publisher_;
         rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr raw_imu_subscription_;
-        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_raw_publisher_;        
+        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_raw_publisher_;
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr raw_laser_scan_subscription_;
+        rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr filtered_laser_scan_publisher_;       
     };
 }
 
--- src/sensor_processor.cpp
+++ src/sensor_processor.cpp
@@ -14,6 +14,20 @@ namespace SensorProcessor
 
         unfiltered_img_raw_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(
             "/camera/unfiltered_image_raw", rclcpp::SensorDataQoS());
+
+        raw_depth_img_raw_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/kohm/depth_image_raw", 10,
+            std::bind(&SensorProcessor::raw_depth_img_raw_callback, this, std::placeholders::_1));
+
+        unfiltered_depth_img_raw_publisher_ = this->create_publisher<sensor_msgs::msg::Image>(
+            "/camera/unfiltered_depth_image_raw", rclcpp::SensorDataQoS());
+
+        raw_laser_scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "/lidar/unfiltered_scan", 10,
+            std::bind(&SensorProcessor::raw_laser_callback, this, std::placeholders::_1));
+
+        filtered_laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>(
+            "/lidar/filtered_scan", rclcpp::SensorDataQoS());
             
         raw_imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
             "/robot/imu", 10,
@@ -29,12 +43,24 @@ namespace SensorProcessor
         msg->header.frame_id = "camera_link";
         unfiltered_img_raw_publisher_->publish(*msg);
     }
+
+    void SensorProcessor::raw_depth_img_raw_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+    {
+        msg->header.frame_id = "camera_link";
+        unfiltered_img_raw_publisher_->publish(*msg);
+    }
      /// Fixes image raw frames
     void SensorProcessor::raw_imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
     {
         msg->header.frame_id = "imu_link";
         imu_raw_publisher_->publish(*msg);
-    }   
+    }
+
+    void SensorProcessor::raw_laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        msg->header.frame_id = "laser_link";
+        filtered_laser_scan_publisher_->publish(*msg);
+    }    
 }
 
 int main(int argc, char *argv[])
